bd13fba73d0b1347fd416adb2c36d6c588efbfb8
aguzpos332 <aguzpos332@g.educaand.es>
2025-12-05 18:21:14 +0100

    Versión 2

--- Proyecto8ED/src/principal/GestorReservas.java
+++ Proyecto8ED/src/principal/GestorReservas.java
@@ -1,9 +1,24 @@
 package principal;
+import objetos.Usuario;
+import objetos.Vuelo;
 public class GestorReservas {
 	public void principal() {
+		
 		/*
-		Realizar funciones que permitan que el usuario -> Hacer una reserva y que se almacene y una función para borrar la reserva (Versión 2) y por último en la versión 3 poder imprimir las reservas con otra función
+		Realizar funciones que permitan que el usuario -> función para borrar la reserva (Versión 2) y por último en la versión 3 poder imprimir las reservas con otra función
 		*/
+	}
+	public void borrarReserva(Usuario u, Vuelo v) {
+		boolean isDelete=false;
+		for (int i = 0; i < u.getReservas().length; i++) {
+			if (v.getIdVuelo() == u.getReservas()[i]) {
+				u.getReservas()[i] = "";
+				isDelete=true;
+			}
+		}
+		if (isDelete == false) {
+			System.out.println("Este usuario no tiene ese vuelo reservado");
+		}
 	}
 	public static void main(String[] args) {
 		new GestorReservas().principal();
