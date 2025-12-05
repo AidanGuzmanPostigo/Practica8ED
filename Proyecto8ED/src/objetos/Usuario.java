e1f9956f0495a7455aec430f237208119fbb348a
rlobmon782 <rlobmon782@g.educaand.es>
2025-12-05 17:11:59 +0100

    Version 2 rama Rocio

--- Proyecto8ED/src/objetos/Usuario.java
+++ Proyecto8ED/src/objetos/Usuario.java
@@ -2,7 +2,7 @@ package objetos;
 
 public class Usuario {
 	/*
-	 * TODO: Versión 2 -> Añadir getters y setter, Versión 3 -> Añadir ToString.
+	 * TODO: Versión 3 -> Añadir ToString.
 	 */
 	private String dni, nom, fechNac;
 	private int telf;
@@ -14,5 +14,47 @@ public class Usuario {
 		this.fechNac = fechNac;
 		this.telf = telf;
 		reservas = new String[20];
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getFechNac() {
+		return fechNac;
+	}
+
+	public int getTelf() {
+		return telf;
+	}
+
+	public StringBuilder getReservas() {
+		StringBuilder s = new StringBuilder();
+		if (s.isEmpty()) {
+			return s.append("Este usuario no tiene reservas");
+		} else {
+			for (int i = 0; i < reservas.length; i++) {
+				if (reservas[i] != null || reservas[i] != "") {
+					s.append(reservas[i] + ", ");
+				}
+			}
+		}
+		return s;
+	}
+
+	public void setReservas(String s) {
+		int i = 0;
+		boolean isRegister = false;
+		do {
+			if (reservas[i].isEmpty() || reservas[i] == "") {
+				reservas[i] = s;
+				isRegister = true;
+			}
+			i++;
+		} while (i < reservas.length || isRegister == false);
 	}
 }
