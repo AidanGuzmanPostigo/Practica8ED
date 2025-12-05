af2d065e11509d8245ed3eece973b689e6bfb0a3
rlobmon782 <rlobmon782@g.educaand.es>
2025-12-05 19:57:34 +0100

    Ultimo commit antes del rebase - rama Rocio

--- Proyecto8ED/src/objetos/Usuario.java
+++ Proyecto8ED/src/objetos/Usuario.java
@@ -29,37 +29,22 @@ public class Usuario {
 		return telf;
 	}
 
-	public StringBuilder getReservas() {
-		StringBuilder s = new StringBuilder();
-		if (s.isEmpty()) {
-			return s.append("Este usuario no tiene reservas");
-		} else {
-			for (int i = 0; i < reservas.length; i++) {
-				if (reservas[i] != null || reservas[i] != "") {
-					s.append(reservas[i] + ", ");
-				}
-			}
-		}
-		return s;
-	}
-
-	public void setReservas(String s) {
-		int i = 0;
-		boolean isRegister = false;
-		do {
-			if (reservas[i].isEmpty() || reservas[i] == "") {
-				reservas[i] = s;
-				isRegister = true;
-			}
-			i++;
-		} while (i < reservas.length || isRegister == false);
+	public String[] getReservas() {
+		return reservas;
 	}
 
 	@Override
 	public String toString() {
 		StringBuilder s = new StringBuilder();
-		s.append(String.format("DNI: %s\nNombre: %s\nFecha de Nacimiento: %s\nTeléfono: %d\nReservas: %d", dni, nom,
-				fechNac, telf, getReservas().toString()));
-		return s.toString();
+		for (int i = 0; i < reservas.length; i++) {
+			if (reservas[i] != null && reservas[i] != "") {
+				s.append(reservas[i] + ", ");
+			}
+		}
+		if (s.isEmpty()) {
+			s.append("Este usuario no tiene reservas");
+		}
+		return String.format("DNI: %s\nNombre: %s\nFecha de Nacimiento: %s\nTeléfono: %d\nReservas: %s", dni, nom,
+				fechNac, telf, s.toString());
 	}
 }
