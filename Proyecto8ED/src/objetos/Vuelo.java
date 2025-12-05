c7488485a831794d9c81b759eba0890ad1138c4f
Alex <Alex@DESKTOP-4HQJO53>
2025-12-05 17:52:00 +0100

    Version 1 - rama Pedro

--- Proyecto8ED/src/objetos/Vuelo.java
+++ Proyecto8ED/src/objetos/Vuelo.java
@@ -1,6 +1,17 @@
 package objetos;
+
 public class Vuelo {
-/*TODO: Hay que hacer un constructor con esta clase que contenga los siguientes atributos: IdVuelo, Origen, Destino, CapacidadTotal, PlazasOcupadas
-Hacerlo por versiones: Versión 1 -> Solo constructor, Versión 2 -> Añadir getters y setter, Versión 3 -> Añadir ToString y un método que devuelva cuantas plazas hay libres
-*/
+	/*
+	 * TODO: Hacerlo por versiones: Versión 2 -> Añadir getters y setter, Versión 3
+	 * -> Añadir ToString y un método que devuelva cuantas plazas hay libres
+	 */
+	private String idVuelo, origen, destino;
+	private int capacidadTotal, plazasOcupadas;
+
+	public Vuelo(String idVuelo, String origen, String destino, int capacidadTotal) {
+		this.idVuelo = idVuelo;
+		this.origen = origen;
+		this.destino = destino;
+		this.capacidadTotal = capacidadTotal;
+	}
 }
